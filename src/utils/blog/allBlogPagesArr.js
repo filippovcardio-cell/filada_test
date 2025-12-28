91fce1804169ea3c7938bf2b61eb2dc9e17e81d3
Igor <filippovcardio@gmail.com>
2025-12-28 15:41:08 +0200

    Add new blog article and update blog hub

--- src/utils/blog/allBlogPagesArr.js
+++ src/utils/blog/allBlogPagesArr.js
@@ -1,27 +1,33 @@
-import blefaroPreview from '../../assets/images/blog/blefaroPreview.jpg'
-import septoPreview from '../../assets/images/blog/septoPreview.JPG'
-import howToChoosePlasticSurgerPreview from '../../assets/images/blog/howToChoosePlasticSurgerPreview.JPG'
-import howtolosebellyfatPreview from '../../assets/images/blog/howtolosebellyfatPreview.webp'
+import blefaroPreview from "../../assets/images/blog/blefaroPreview.jpg";
+import septoPreview from "../../assets/images/blog/septoPreview.JPG";
+import howToChoosePlasticSurgerPreview from "../../assets/images/blog/howToChoosePlasticSurgerPreview.JPG";
+import howtolosebellyfatPreview from "../../assets/images/blog/howtolosebellyfatPreview.webp";
+import zbilshennyaGrudeiPreview from "../../assets/images/blog/zbilshennya-grudei-2.webp";
 
 export const allBlogPagesArr = [
-      {
-          path: "/plastika-povik-blefaroplastika-koli-potribna-i-yak-prohodit/",
-          previewImage: blefaroPreview,
-          title: "Пластика повік (блефаропластика): коли потрібна і як проходить",
-      },
-      {
-          path: "/septoplastika-pokazannya-riziki-ta-period-vidnovlennya/",
-          previewImage: septoPreview,
-          title: "Септопластика: показання, ризики та період відновлення",
-      },
-      {
-          path: "/yak-vibrati-plastichnogo-hirurga-kriteriyiv-dlya-prijnyattya-rishennya/",
-          previewImage: howToChoosePlasticSurgerPreview,
-          title: "Як вибрати пластичного хірурга: критерії для прийняття рішення",
-      },
-      {
-          path: "/yak-pribrati-zhivit/",
-          previewImage: howtolosebellyfatPreview,
-          title: "Як прибрати живіт?",
-      },
-]
+  {
+    path: "/plastika-povik-blefaroplastika-koli-potribna-i-yak-prohodit/",
+    previewImage: blefaroPreview,
+    title: "Пластика повік (блефаропластика): коли потрібна і як проходить",
+  },
+  {
+    path: "/septoplastika-pokazannya-riziki-ta-period-vidnovlennya/",
+    previewImage: septoPreview,
+    title: "Септопластика: показання, ризики та період відновлення",
+  },
+  {
+    path: "/yak-vibrati-plastichnogo-hirurga-kriteriyiv-dlya-prijnyattya-rishennya/",
+    previewImage: howToChoosePlasticSurgerPreview,
+    title: "Як вибрати пластичного хірурга: критерії для прийняття рішення",
+  },
+  {
+    path: "/yak-pribrati-zhivit/",
+    previewImage: howtolosebellyfatPreview,
+    title: "Як прибрати живіт?",
+  },
+  {
+    path: "/10-pytan-pro-mammoplastiku-yaki-vi-boyalisya-postaviti-hirurgu/",
+    previewImage: zbilshennyaGrudeiPreview,
+    title: "Збільшення грудей: топ-10 питань, які соромляться задати на консультації",
+  },
+];
--- src/utils/blog/blog.js
+++ src/utils/blog/blog.js
@@ -1,107 +1,151 @@
-import { text } from "framer-motion/client";
 import { blogBlefaroplastyText } from "./blogBlefaroplastyText";
-import oKosenkoAvatar from '../../assets/images/oKosenkoAvatar.png'
-import hMekhynAvatar from '../../assets/images/hMekhynAvatar.png'
+import oKosenkoAvatar from "../../assets/images/oKosenkoAvatar.png";
+import hMekhynAvatar from "../../assets/images/hMekhynAvatar.png";
 import { blogSeptoplastyText } from "./blogSeptoplastyText";
-import septhoPhoto1 from '../../assets/images/blog/Septho.jpg'
+import septhoPhoto1 from "../../assets/images/blog/Septho.jpg";
 import { blogHowToChoosePlasticSurger } from "./blogHowToChoosePlasticSurger";
-import plasticBlogPhoto1 from '../../assets/images/blog/plasticBlogPhoto1.JPG'
-import plasticBlogPhoto2 from '../../assets/images/blog/plasticBlogPhoto2.JPG'
-import plasticBlogPhoto3 from '../../assets/images/blog/plasticBlogPhoto3.JPG'
+import plasticBlogPhoto1 from "../../assets/images/blog/plasticBlogPhoto1.JPG";
+import plasticBlogPhoto2 from "../../assets/images/blog/plasticBlogPhoto2.JPG";
+import plasticBlogPhoto3 from "../../assets/images/blog/plasticBlogPhoto3.JPG";
 import { blogHowToLoseBellyFat } from "./blogHowToLoseBellyFat";
-import blogHowToLoseBellyFatPhoto1 from '../../assets/images/blog/blogHowToLoseBellyFatPhoto1.webp'
-import blogHowToLoseBellyFatPhoto2 from '../../assets/images/blog/blogHowToLoseBellyFatPhoto2.webp'
+import blogHowToLoseBellyFatPhoto1 from "../../assets/images/blog/blogHowToLoseBellyFatPhoto1.webp";
+import blogHowToLoseBellyFatPhoto2 from "../../assets/images/blog/blogHowToLoseBellyFatPhoto2.webp";
+import { blogBreastAugmentationTop10QuestionsText } from "./blogBreastAugmentationTop10QuestionsText";
+import breastAugmentationPreview1 from "../../assets/images/blog/zbilshennya-grudei-1.webp";
+import breastAugmentationPreview2 from "../../assets/images/blog/zbilshennya-grudei-2.webp";
+import breastAugmentationPreview3 from "../../assets/images/blog/zbilshennya-grudei-3.webp";
 
 export const blogArr = [
-    {
-        path: "/plastika-povik-blefaroplastika-koli-potribna-i-yak-prohodit/",
-        metaTitle: "Блефаропластика: що це та кому необхідна? | Блог МЦ Filada",
-        metaDescription: "Дізнайтесь, що таке блефаропластика, кому вона рекомендована та як проходить процедура. Все про показання, переваги та етапи хірургічного втручання — у блозі МЦ Filada.",
-        metaUrl:"https://filada.com.ua/blog/plastika-povik-blefaroplastika-koli-potribna-i-yak-prohodit/",
-        coverDescription: "Блефаропластика",
-        text: blogBlefaroplastyText,
-        imagesArr: null,
-        author: {
-            name: "Олександр Косенко",
-            job: "Пластичний хірург",
-            image: oKosenkoAvatar,
-            link: "/doctors/oleksandr-kosenko",
-        },
-        hrefDetails: "/plastic-surgery/",
+  {
+    path: "/plastika-povik-blefaroplastika-koli-potribna-i-yak-prohodit/",
+    metaTitle: "Блефаропластика: що це та кому необхідна? | Блог МЦ Filada",
+    metaDescription:
+      "Дізнайтесь, що таке блефаропластика, кому вона рекомендована та як проходить процедура. Все про показання, переваги та етапи хірургічного втручання — у блозі МЦ Filada.",
+    metaUrl:
+      "https://filada.com.ua/blog/plastika-povik-blefaroplastika-koli-potribna-i-yak-prohodit/",
+    coverDescription: "Блефаропластика",
+    text: blogBlefaroplastyText,
+    imagesArr: null,
+    author: {
+      name: "Олександр Косенко",
+      job: "Пластичний хірург",
+      image: oKosenkoAvatar,
+      link: "/doctors/oleksandr-kosenko",
     },
-    {
-        path: "/septoplastika-pokazannya-riziki-ta-period-vidnovlennya/",
-        metaTitle: "Що таке септопластика: все про операцію | Блог МЦ Filada",
-        metaDescription: "Дізнайтесь, що таке септопластика, кому показана операція, як проходить відновлення та які ризики можуть виникнути. Все про оперативне лікування викривлення носової перегородки — у блозі МЦ Filada.",
-        metaUrl:"https://filada.com.ua/blog/septoplastika-pokazannya-riziki-ta-period-vidnovlennya/",
-        coverDescription: "Септопластика",
-        text: blogSeptoplastyText,
-        imagesArr: [
-            {
-                src: septhoPhoto1,
-                alt: "Септопластика фото",
-            },
-        ],
-        author: {
-            name: "Ганна Мекшун",
-            job: "Лікар-отоларинголог",
-            image: hMekhynAvatar,
-            link: "/doctors/hanna-mekshyn",
-        },
-        hrefDetails: "/otolaryngology/",
+    hrefDetails: "/plastic-surgery/",
+  },
+  {
+    path: "/septoplastika-pokazannya-riziki-ta-period-vidnovlennya/",
+    metaTitle: "Що таке септопластика: все про операцію | Блог МЦ Filada",
+    metaDescription:
+      "Дізнайтесь, що таке септопластика, кому показана операція, як проходить відновлення та які ризики можуть виникнути. Все про оперативне лікування викривлення носової перегородки — у блозі МЦ Filada.",
+    metaUrl:
+      "https://filada.com.ua/blog/septoplastika-pokazannya-riziki-ta-period-vidnovlennya/",
+    coverDescription: "Септопластика",
+    text: blogSeptoplastyText,
+    imagesArr: [
+      {
+        src: septhoPhoto1,
+        alt: "Септопластика фото",
+      },
+    ],
+    author: {
+      name: "Ганна Мекшун",
+      job: "Лікар-отоларинголог",
+      image: hMekhynAvatar,
+      link: "/doctors/hanna-mekshyn",
     },
-    {
-        path: "/yak-vibrati-plastichnogo-hirurga-kriteriyiv-dlya-prijnyattya-rishennya/",
-        metaTitle: "Пластична хірургія: за проти коррекції тіла | Блог МЦ Filada",
-        metaDescription: "Дізнайтеся все про пластичну хірургію: переваги, ризики, види пластичних операцій, вартість та як обрати хірурга для корекції тіла. Відповідаємо чесно — за і проти втручань у зовнішність.",
-        metaUrl:"https://filada.com.ua/blog/yak-vibrati-plastichnogo-hirurga-kriteriyiv-dlya-prijnyattya-rishennya/",
-        coverDescription: "Як вибрати пластичного хірурга",
-        text: blogHowToChoosePlasticSurger,
-        imagesArr: [
-            {
-                src: plasticBlogPhoto1,
-                alt: "небезпеки пластичної хірургії",
-            },
-            {
-                src: plasticBlogPhoto2,
-                alt: "пластичний хірург це",
-            },
-            {
-                src: plasticBlogPhoto3,
-                alt: "перша пластична операція",
-            },
-        ],
-        author: {
-            name: "Олександр Косенко",
-            job: "Пластичний хірург",
-            image: oKosenkoAvatar,
-            link: "/doctors/oleksandr-kosenko",
-        },
-        hrefDetails: "/plastic-surgery/",
+    hrefDetails: "/otolaryngology/",
+  },
+  {
+    path: "/yak-vibrati-plastichnogo-hirurga-kriteriyiv-dlya-prijnyattya-rishennya/",
+    metaTitle: "Пластична хірургія: за проти коррекції тіла | Блог МЦ Filada",
+    metaDescription:
+      "Дізнайтеся все про пластичну хірургію: переваги, ризики, види пластичних операцій, вартість та як обрати хірурга для корекції тіла. Відповідаємо чесно — за і проти втручань у зовнішність.",
+    metaUrl:
+      "https://filada.com.ua/blog/yak-vibrati-plastichnogo-hirurga-kriteriyiv-dlya-prijnyattya-rishennya/",
+    coverDescription: "Як вибрати пластичного хірурга",
+    text: blogHowToChoosePlasticSurger,
+    imagesArr: [
+      {
+        src: plasticBlogPhoto1,
+        alt: "небезпеки пластичної хірургії",
+      },
+      {
+        src: plasticBlogPhoto2,
+        alt: "пластичний хірург це",
+      },
+      {
+        src: plasticBlogPhoto3,
+        alt: "перша пластична операція",
+      },
+    ],
+    author: {
+      name: "Олександр Косенко",
+      job: "Пластичний хірург",
+      image: oKosenkoAvatar,
+      link: "/doctors/oleksandr-kosenko",
     },
-    {
-        path: "/yak-pribrati-zhivit/",
-        metaTitle: "Як позбутися живота? | Блог МЦ Filada",
-        metaDescription: "Дізнайтеся, як ефективно прибрати живіт: поєднання дієти, вправ і медичних рішень. Читайте поради експертів клініки Filada — шлях до плоского живота починається тут!",
-        metaUrl:"https://filada.com.ua/blog/yak-pribrati-zhivit/",
-        coverDescription: "Як позбутися живота",
-        text: blogHowToLoseBellyFat,
-        imagesArr: [
-            {
-                src: blogHowToLoseBellyFatPhoto1,
-                alt: "blogHowToLoseBellyFatPhoto1",
-            },
-            {
-                src: blogHowToLoseBellyFatPhoto2,
-                alt: "як позбутися жиру на животі",
-            },
-        ],
-        author: {
-            name: "Олександр Косенко",
-            job: "Пластичний хірург",
-            image: oKosenkoAvatar,
-            link: "/doctors/oleksandr-kosenko",
-        },
-        hrefDetails: "/plastic-surgery/",
+    hrefDetails: "/plastic-surgery/",
+  },
+  {
+    path: "/yak-pribrati-zhivit/",
+    metaTitle: "Як позбутися живота? | Блог МЦ Filada",
+    metaDescription:
+      "Дізнайтеся, як ефективно прибрати живіт: поєднання дієти, вправ і медичних рішень. Читайте поради експертів клініки Filada — шлях до плоского живота починається тут!",
+    metaUrl: "https://filada.com.ua/blog/yak-pribrati-zhivit/",
+    coverDescription: "Як позбутися живота",
+    text: blogHowToLoseBellyFat,
+    imagesArr: [
+      {
+        src: blogHowToLoseBellyFatPhoto1,
+        alt: "blogHowToLoseBellyFatPhoto1",
+      },
+      {
+        src: blogHowToLoseBellyFatPhoto2,
+        alt: "як позбутися жиру на животі",
+      },
+    ],
+    author: {
+      name: "Олександр Косенко",
+      job: "Пластичний хірург",
+      image: oKosenkoAvatar,
+      link: "/doctors/oleksandr-kosenko",
     },
-]
+    hrefDetails: "/plastic-surgery/",
+  },
+
+  {
+    path: "/10-pytan-pro-mammoplastiku-yaki-vi-boyalisya-postaviti-hirurgu/",
+    metaTitle:
+      "10 питань про маммопластику, які ви боялися поставити хірургу | Блог МЦ Filada",
+    metaDescription:
+      "Чесні відповіді на найделікатніші питання про збільшення грудей: від чутливості та грудного вигодовування до болю й термінів служби імплантів. Все, що ви хотіли знати, але соромилися запитати.",
+    metaUrl:
+      "https://filada.com.ua/blog/10-pytan-pro-mammoplastiku-yaki-vi-boyalisya-postaviti-hirurgu/",
+    coverDescription: "10 питань про маммопластику",
+    text: blogBreastAugmentationTop10QuestionsText,
+    imagesArr: [
+      {
+        src: breastAugmentationPreview1,
+        alt: "Збільшення грудей",
+      },
+       {
+        src: breastAugmentationPreview2,
+        alt: "Збільшення грудей",
+      },
+       {
+        src: breastAugmentationPreview3,
+        alt: "Збільшення грудей",
+      },
+    ],
+    author: {
+      name: "Олександр Косенко",
+      job: "Пластичний хірург",
+      image: oKosenkoAvatar,
+      link: "/doctors/oleksandr-kosenko",
+    },
+    hrefDetails: "/plastic-surgery/",
+  },
+];
+
